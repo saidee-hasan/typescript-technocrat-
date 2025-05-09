7891ba5c8198dcdfc36584c6eaed30cabc3610aa
Saidee Hasan <mdsaideehasan@gmail.com>
2025-05-09 08:28:21 +0600

    destring

---
+++ module/src/1.9.ts
@@ -0,0 +1,18 @@
+const students1: {
+    name:string,
+    age:number,
+    gender: string,
+    contactNo: number,
+    address: string
+    
+}={
+    name:"Saidee" ,
+    age:20,
+    gender:"Male",
+    contactNo: 01765895325,
+    address:'Gaibandha '
+
+    
+
+
+}
